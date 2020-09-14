ded4368aa9885d079494e0b54d77dd9d6de9c8c3
mhaboali <m.aboali1117@gmail.com>
2020-09-14 13:56:56 +0200

    Revert the local_eval to the previous version to run it on local only

--- turtlebot3_gazebo/src/eval.py
+++ turtlebot3_gazebo/src/eval.py
@@ -15,7 +15,7 @@ import sys
 import psutil as ps
 import subprocess as sp
 import os
-from turtlebot3_msgs.msg import ResourcesList, BwList, HzList
+from turtlebot3_msgs.msg import ResourcesList, BwList
 
 import csv
 from itertools import izip
@@ -26,9 +26,8 @@ resources_pub = rospy.Publisher(platform+'/resources_usage', ResourcesList, queu
 
 bw_pub = rospy.Publisher(platform+'/bw', BwList, queue_size=10)
 
-hz_pub = rospy.Publisher(platform+'/hz', HzList, queue_size=10)
-
-
+resources_usage = ResourcesList()
+bw_values = BwList()
 
 def get_gpu_memory():
   _output_to_list = lambda x: x.decode('ascii').split('\n')[:-1]
@@ -44,10 +43,107 @@ def get_gpu_memory():
 # seed random number generator
 seed(1)
 
+points_0 = []
+points_1 = []
+points_2 = []
 
+def movebase_client_cmd(ns, pos):
+
+   # Create an action client called "move_base" with action definition file "MoveBaseAction"
+    client = actionlib.SimpleActionClient(ns+'move_base',MoveBaseAction)
+ 
+   # Waits until the action server has started up and started listening for goals.
+    client.wait_for_server()
+
+   # Creates a new goal with the MoveBaseGoal constructor
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.header.stamp = rospy.Time.now()
+   # Move 0.5 meters forward along the x axis of the "map" coordinate frame 
+    goal.target_pose.pose.position.x = pos[0]
+    goal.target_pose.pose.position.y = pos[1]
+   # No rotation of the mobile base frame w.r.t. map frame
+    goal.target_pose.pose.orientation.w = 1.0
+
+   # Sends the goal to the action server.
+    client.send_goal(goal)
+   # Waits for the server to finish performing the action.
+    wait = client.wait_for_result()
+   # If the result doesn't arrive, assume the Server is not available
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+    # Result of executing the action
+        return client.get_result()   
+
+def exec_cmd(ns,pos):
+    try:
+        result = movebase_client_cmd(ns, pos)
+        if result:
+            rospy.loginfo(ns + ":       Goal execution done!")
+            return result
+    
+    except rospy.ROSInterruptException:
+        rospy.loginfo("Navigation finished.")
+
+def exec_robot_points(ns, points):
+    for pnt in points:
+        res = exec_cmd(ns, pnt)        
+
+def gen_x(min, max):
+    return randint(min, max, 3)
+
+def gen_y(min, max):
+   return random.sample(xrange(min, max), 3)
+
+def gen_oneSample_points():
+    mode = randint(0,1,1)
+    points = []
+    x_list = []
+    y_list = []
+    if mode == 0:
+        x_list = gen_x(0,2)
+        y_list = gen_y(-9,9)
+    else:
+        x_list = gen_x(-5,2)
+        y_list = gen_y(-9,0)
+    return zip(x_list,y_list)
+
+def gen_nSamples_points(n):
+    points = []
+    for i in range(0,n):
+        points.append(gen_oneSample_points())
+    print(points)
+    return points
+
+def allocate_points(points):
+    for sample in points:
+        points_0.append(sample[0])
+        points_1.append(sample[1])
+        points_2.append(sample[2])
+
+def write_dataset(stamp, x, y, z):
+    with open(stamp+'.csv', 'wb') as f:
+        writer = csv.writer(f, delimiter = ',')
+        line_num = 0
+        rows = []
+        for (a, b, c) in zip(x, y, z):
+            line_num+=1
+            if line_num == 1: 
+                rows.append([a[0],a[1],a[2], b[0],b[1],b[2],b[3], c[0],c[1],c[2]])
+            else:
+                rows.append([str(a[0]),str(a[1]),str(a[2]), str(b[0]),str(b[1]),str(b[2]),str(b[3]), str(c[0]),str(c[1]),str(c[2])])
+        writer.writerows(rows)
+            # writer.writerows([x.split(',') for x in row_str])
 def main():
     # Initializes a rospy node to let the SimpleActionClient publish and subscribe
     rospy.init_node('local_evaluator')
+    points = gen_nSamples_points(int(sys.argv[1]))
+    allocate_points(points)
+    plan_tb0 = threading.Thread(target=exec_robot_points, args=("/tb3_0/", points_0))
+    plan_tb1 = threading.Thread(target=exec_robot_points, args=("/tb3_1/", points_1))
+    plan_tb2 = threading.Thread(target=exec_robot_points, args=("/tb3_2/", points_2))
     
     bw_scan_0 = rostopic.ROSTopicBandwidth()
     scan_0 = rospy.Subscriber('/tb3_0/scan', rospy.AnyMsg, bw_scan_0.callback)
@@ -87,62 +183,73 @@ def main():
     detectron_2 = rospy.Subscriber('/tb3_2/detectron2_ros/result', rospy.AnyMsg, hz_detectron_2.callback_hz) """
 
 
-    rospy.loginfo("LOCAL >> Starting publishing the dataset in ROS topics ....")
-    try:
-        while not rospy.is_shutdown():
-            global resources_pub, bw_pub, hz_pub
-            resources_usage = ResourcesList()
-            bw_values = BwList()
-            hz_values = HzList()
-            try:
-                resources_usage.header.frame_id = platform
-                resources_usage.header.stamp = rospy.Time.now()
+    rospy.loginfo("Starting now executing the generated goals ....")
+    plan_tb0.start()
+    plan_tb1.start()
+    plan_tb2.start()
 
-                cpu_percent = ps.cpu_percent()
-                resources_usage.cpu = cpu_percent
-                ram_percent = ps.virtual_memory()[2]
-                resources_usage.ram = ram_percent
-                gpu_percent = get_gpu_memory()
-                resources_usage.gpu = gpu_percent
-                
-                resources_pub.publish(resources_usage)
+    global resources_usage, bw_values, resources_pub, bw_pub
+    scan_bw = []    # bandwidth = datasize/sec
+    map_pt = []     # publishTime = 1/pubFreq
+    resources_data = []
+
+    resources_data.append(["RAM(%)", "CPU(%)", "GPU(%"])
+    scan_bw.append(["laser_0_bw(B/sec)", "laser_1_bw(B/sec)", "laser_2_bw(B/sec)"])
+    map_pt.append(["map_0_execT(sec)", "map_1_execT(sec)", "map_2_execT(sec)", "fullMap_execT(sec)"])
+
+    rospy.loginfo("Starting storing the dataset in python lists ....")
+    while not rospy.is_shutdown():
+        global scan_bw, map_pt 
+        #resources_usage.header.frame_id = platform
+        #resources_usage.header.stamp = rospy.Time.now()
+
+        cpu_percent = ps.cpu_percent()
+        #resources_usage.cpu = cpu_percent
+        ram_percent = ps.virtual_memory()[2]
+        #resources_usage.ram = ram_percent
+        gpu_percent = get_gpu_memory()
+        #resources_usage.gpu = gpu_percent
+        
+        resources_data.append([round(ram_percent, 3), round(cpu_percent, 3), round(gpu_percent, 3)])
+        #resources_pub.publish(resources_usage)
+        try:
+            #bw_values.header.frame_id = platform
+            #bw_values.header.stamp = rospy.Time.now()
             
-                bw_values.header.frame_id = platform
-                bw_values.header.stamp = rospy.Time.now()
-                
-                scan_0 = float(bw_scan_0.get_bw())            
-                bw_values.scan_0 = scan_0
-                scan_1 = float(bw_scan_1.get_bw())
-                bw_values.scan_1 = scan_1
-                scan_2 = float(bw_scan_2.get_bw())
-                bw_values.scan_2 = scan_2
-                
+            scan_0 = float(bw_scan_0.get_bw())            
+            #bw_values.scan_0 = scan_0
+            scan_1 = float(bw_scan_1.get_bw())
+            #bw_values.scan_1 = scan_1
+            scan_2 = float(bw_scan_2.get_bw())
+            #bw_values.scan_2 = scan_2
+            
+            scan_bw.append([scan_0, scan_1, scan_2])
 
-                full_map = hz_map.get_hz("/map")[0]
-                hz_values.map = full_map
-                map_1 = hz_map_1.get_hz("/tb3_1/map")[0]
-                hz_values.map_1 = map_1
-                map_2 = hz_map_2.get_hz("/tb3_2/map")[0]
-                hz_values.map_2 = map_2
-                map_0 = hz_map_0.get_hz("/tb3_0/map")[0]
-                hz_values.map_0 = map_0
+            full_map = hz_map.get_hz("/map")[0]
+            #hz_values.map = full_map
+            map_1 = hz_map_1.get_hz("/tb3_1/map")[0]
+            #hz_values.map_1 = map_1
+            map_2 = hz_map_2.get_hz("/tb3_2/map")[0]
+            #hz_values.map_2 = map_2
+            map_0 = hz_map_0.get_hz("/tb3_0/map")[0]
+            #hz_values.map_0 = map_0
+            map_pt.append([round(1.0/map_0, 3), round(1.0/map_1, 3), round(1.0/map_2, 3), round(1.0/full_map, 3)])
 
-                """ bw_values.detectron_0 = bw_detectron_0.get_bw()
-                bw_values.detectron_1 = bw_detectron_1.get_bw()
-                bw_values.detectron_2 = bw_detectron_2.get_bw() """
-                
-                bw_pub.publish(bw_values)
-                hz_pub.publish(hz_values)
-            except:
-                pass
-                # print(type(bw_scan_0.get_bw()))
-                # print(hz_map_0.get_hz("/tb3_0/map"))
-                # hz_map_0.print_hz(["/tb3_0/map"])
-                # print("################################")
-    except KeyboardInterrupt:
-        rospy.loginfo("LOCAL >> Navigation compeleted!")   
 
-    rospy.loginfo("LOCAL >> Now dataset is ready ....")
+            """ bw_values.detectron_0 = bw_detectron_0.get_bw()
+            bw_values.detectron_1 = bw_detectron_1.get_bw()
+            bw_values.detectron_2 = bw_detectron_2.get_bw() """
+            
+            #bw_pub.publish(bw_values)
+        except:
+            pass
+            # print(type(bw_scan_0.get_bw()))
+            # print(hz_map_0.get_hz("/tb3_0/map"))
+            # hz_map_0.print_hz(["/tb3_0/map"])
+            # print("################################")
+    rospy.loginfo("Starting writing the dataset in csv file ....")
+    write_dataset(str(rospy.Time.now().to_nsec()), scan_bw, map_pt, resources_data)     
+    rospy.loginfo("Now dataset is ready ....")
     rospy.spin()
 
 if __name__ == '__main__':
