a377fe8d868353cbc2b5a96745881d2a6aa4bcf3
mhaboali <m.aboali1117@gmail.com>
2020-09-20 14:20:40 +0200

    After adding the maps bandwidths and it works perfectly now

--- turtlebot3_gazebo/src/eval.py
+++ turtlebot3_gazebo/src/eval.py
@@ -122,19 +122,19 @@ def allocate_points(points):
         points_0.append(sample[0])
         points_1.append(sample[1])
         points_2.append(sample[2])
-
-def write_dataset(stamp, x, y, z):
+        
+def write_dataset(stamp, x, y, y_bw, z):
     with open(stamp+'_local.csv', 'wb') as f:
         writer = csv.writer(f, delimiter = ',')
         line_num = 0
         rows = []
-        for (a, b, c) in zip(x, y, z):
+        for (a, b, b_bw, c) in zip(x, y, y_bw, z):
             line_num+=1
             if line_num == 1: 
-                rows.append([a[0],a[1],a[2], b[0],b[1],b[2],b[3], c[0],c[1],c[2]])
+                rows.append([a[0],a[1],a[2], b[0],b[1],b[2],b[3], b_bw[0],b_bw[1],b_bw[2],b_bw[3], c[0],c[1],c[2]])
             else:
-                rows.append([str(a[0]),str(a[1]),str(a[2]), str(b[0]),str(b[1]),str(b[2]),str(b[3]), str(c[0]),str(c[1]),str(c[2])])
-        print(len(rows))
+                rows.append([str(a[0]),str(a[1]),str(a[2]), str(b[0]),str(b[1]),str(b[2]),str(b[3]), str(b_bw[0]),str(b_bw[1]),str(b_bw[2]),str(b_bw[3]), str(c[0]),str(c[1]),str(c[2])])
+        print(rows)
         writer.writerows(rows)
             # writer.writerows([x.split(',') for x in row_str])
 def main():
@@ -145,44 +145,31 @@ def main():
     plan_tb0 = threading.Thread(target=exec_robot_points, args=("/tb3_0/", points_0))
     plan_tb1 = threading.Thread(target=exec_robot_points, args=("/tb3_1/", points_1))
     plan_tb2 = threading.Thread(target=exec_robot_points, args=("/tb3_2/", points_2))
-    
+
     bw_scan_0 = rostopic.ROSTopicBandwidth()
     scan_0 = rospy.Subscriber('/tb3_0/scan', rospy.AnyMsg, bw_scan_0.callback)
     bw_scan_1 = rostopic.ROSTopicBandwidth()
     scan_1 = rospy.Subscriber('/tb3_1/scan', rospy.AnyMsg, bw_scan_1.callback)
     bw_scan_2 = rostopic.ROSTopicBandwidth()
-    scan_2 = rospy.Subscriber('/tb3_2/scan', rospy.AnyMsg, bw_scan_2.callback)
+    scan_2 = rospy.Subscriber('/tb3_2/scan', rospy.AnyMsg, bw_scan_2.callback) 
 
-    """ bw_img_0 = rostopic.ROSTopicBandwidth()
-    img_0 = rospy.Subscriber('/tb3_0camera/rgb/image_raw/compressed', rospy.AnyMsg, bw_img_0.callback)
-    bw_img_1 = rostopic.ROSTopicBandwidth()
-    img_1 = rospy.Subscriber('/tb3_1camera/rgb/image_raw/compressed', rospy.AnyMsg, bw_img_1.callback)
-    bw_img_2 = rostopic.ROSTopicBandwidth()
-    img_2 = rospy.Subscriber('/tb3_2camera/rgb/image_raw/compressed', rospy.AnyMsg, bw_img_2.callback) """
-
-    """ bw_detectron_0 = rostopic.ROSTopicBandwidth()
-    detectron_0 = rospy.Subscriber('/tb3_0/detectron2_ros/result', rospy.AnyMsg, bw_detectron_0.callback)
-    bw_detectron_1 = rostopic.ROSTopicBandwidth()
-    detectron_1 = rospy.Subscriber('/tb3_1/detectron2_ros/result', rospy.AnyMsg, bw_detectron_1.callback)
-    bw_detectron_2 = rostopic.ROSTopicBandwidth()
-    detectron_2 = rospy.Subscriber('/tb3_2/detectron2_ros/result', rospy.AnyMsg, bw_detectron_2.callback) """
-
+    bw_map = rostopic.ROSTopicBandwidth()
+    full_map = rospy.Subscriber('/map', rospy.AnyMsg, bw_map.callback)
+    bw_map_0 = rostopic.ROSTopicBandwidth()
+    map_0 = rospy.Subscriber('/tb3_0/map', rospy.AnyMsg, bw_map_0.callback)
+    bw_map_1 = rostopic.ROSTopicBandwidth()
+    map_1 = rospy.Subscriber('/tb3_1/map', rospy.AnyMsg, bw_map_1.callback)
+    bw_map_2 = rostopic.ROSTopicBandwidth()
+    map_2 = rospy.Subscriber('/tb3_2/map', rospy.AnyMsg, bw_map_2.callback)
+    
     hz_map = rostopic.ROSTopicHz(-1)
-    map_0 = rospy.Subscriber('/map', rospy.AnyMsg, hz_map.callback_hz, callback_args='/map')
+    hzmap_ = rospy.Subscriber('/map', rospy.AnyMsg, hz_map.callback_hz, callback_args='/map')
     hz_map_0 = rostopic.ROSTopicHz(-1)
-    map_0 = rospy.Subscriber('/tb3_0/map', rospy.AnyMsg, hz_map_0.callback_hz, callback_args='/tb3_0/map')
+    hzmap_0 = rospy.Subscriber('/tb3_0/map', rospy.AnyMsg, hz_map_0.callback_hz, callback_args='/tb3_0/map')
     hz_map_1 = rostopic.ROSTopicHz(-1)
-    map_1 = rospy.Subscriber('/tb3_1/map', rospy.AnyMsg, hz_map_1.callback_hz, callback_args='/tb3_1/map')
+    hzmap_1 = rospy.Subscriber('/tb3_1/map', rospy.AnyMsg, hz_map_1.callback_hz, callback_args='/tb3_1/map')
     hz_map_2 = rostopic.ROSTopicHz(-1)
-    map_2 = rospy.Subscriber('/tb3_2/map', rospy.AnyMsg, hz_map_2.callback_hz, callback_args='/tb3_2/map')
-
-    """ hz_detectron_0 = rostopic.ROSTopicHz(-1)
-    detectron_0 = rospy.Subscriber('/tb3_0/detectron2_ros/result', rospy.AnyMsg, hz_detectron_0.callback_hz)
-    hz_detectron_1 = rostopic.ROSTopicHz(-1)
-    detectron_1 = rospy.Subscriber('/tb3_1/detectron2_ros/result', rospy.AnyMsg, hz_detectron_1.callback_hz)
-    hz_detectron_2 = rostopic.ROSTopicHz(-1)
-    detectron_2 = rospy.Subscriber('/tb3_2/detectron2_ros/result', rospy.AnyMsg, hz_detectron_2.callback_hz) """
-
+    hzmap_2 = rospy.Subscriber('/tb3_2/map', rospy.AnyMsg, hz_map_2.callback_hz, callback_args='/tb3_2/map')
 
     rospy.loginfo("LOCAL >> Starting now executing the generated goals ....")
     plan_tb0.start()
@@ -191,70 +178,52 @@ def main():
 
     global resources_usage, bw_values, resources_pub, bw_pub
     scan_bw = []    # bandwidth = datasize/sec
-    map_pt = []     # publishTime = 1/pubFreq
+    map_bw = []     # publishTime = 1/pubFreq
+    map_pt = []
     resources_data = []
 
-    resources_data.append(["RAM(%)", "CPU(%)", "GPU(%"])
-    scan_bw.append(["laser_0_bw(B/sec)", "laser_1_bw(B/sec)", "laser_2_bw(B/sec)"])
-    map_pt.append(["map_0_execT(sec)", "map_1_execT(sec)", "map_2_execT(sec)", "fullMap_execT(sec)"])
+    resources_data.append([platform + "_RAM(%)", platform + "_CPU(%)", platform + "_GPU(%)"])
+    scan_bw.append([platform + "_laser_0_bw(KB/sec)", platform + "_laser_1_bw(KB/sec)", platform + "_laser_2_bw(KB/sec)"])
+    map_bw.append([platform + "_map_0_bw(KB/sec)", platform + "_map_1_bw(KB/sec)", platform + "_map_2_bw(KB/sec)", platform + "_fullMap_bw(KB/sec)"])
+    map_pt.append([platform + "_map_0_execT(sec)", platform + "_map_1_execT(sec)", platform + "_map_2_execT(sec)", platform + "_fullMap_execT(sec)"])
 
     rospy.loginfo("LOCAL >> Starting storing the dataset in python lists ....")
     try:
-        while not rospy.is_shutdown():
-            global scan_bw, map_pt 
+        while not rospy.is_shutdown():            
             try:
-                #resources_usage.header.frame_id = platform
-                #resources_usage.header.stamp = rospy.Time.now()
-
-                cpu_percent = ps.cpu_percent()
-                #resources_usage.cpu = cpu_percent
+                cpu_percent = ps.cpu_percent(interval=0.1)                
                 ram_percent = ps.virtual_memory()[2]
-                #resources_usage.ram = ram_percent
                 gpu_percent = get_gpu_memory()
-                #resources_usage.gpu = gpu_percent
                 
                 resources_data.append([round(ram_percent, 3), round(cpu_percent, 3), round(gpu_percent, 3)])
-                #resources_pub.publish(resources_usage)
-            
-                #bw_values.header.frame_id = platform
-                #bw_values.header.stamp = rospy.Time.now()
-                
-                scan_0 = float(bw_scan_0.get_bw())            
-                #bw_values.scan_0 = scan_0
-                scan_1 = float(bw_scan_1.get_bw())
-                #bw_values.scan_1 = scan_1
-                scan_2 = float(bw_scan_2.get_bw())
-                #bw_values.scan_2 = scan_2
+               
+                scan_0 = float(bw_scan_0.get_bw() / 1000.0)            
+                scan_1 = float(bw_scan_1.get_bw() / 1000.0)
+                scan_2 = float(bw_scan_2.get_bw() / 1000.0)
                 
                 scan_bw.append([scan_0, scan_1, scan_2])
 
-                full_map = hz_map.get_hz("/map")[0]
-                #hz_values.map = full_map
-                map_1 = hz_map_1.get_hz("/tb3_1/map")[0]
-                #hz_values.map_1 = map_1
-                map_2 = hz_map_2.get_hz("/tb3_2/map")[0]
-                #hz_values.map_2 = map_2
-                map_0 = hz_map_0.get_hz("/tb3_0/map")[0]
-                #hz_values.map_0 = map_0
+                full_map = float(bw_map.get_bw() / 1000.0)
+                map_0 = float(bw_map_0.get_bw() / 1000.0)            
+                map_1 = float(bw_map_1.get_bw() / 1000.0)
+                map_2 = float(bw_map_2.get_bw() / 1000.0)
+                map_bw.append([round(map_0, 3), round(map_1, 3), round(map_2, 3), round(full_map, 3)])
+
+                full_map = hz_map.get_hz("/map")[0]                
+                map_1 = hz_map_1.get_hz("/tb3_1/map")[0]                
+                map_2 = hz_map_2.get_hz("/tb3_2/map")[0]                
+                map_0 = hz_map_0.get_hz("/tb3_0/map")[0]               
                 map_pt.append([round(1.0/map_0, 3), round(1.0/map_1, 3), round(1.0/map_2, 3), round(1.0/full_map, 3)])
-
-
-                """ bw_values.detectron_0 = bw_detectron_0.get_bw()
-                bw_values.detectron_1 = bw_detectron_1.get_bw()
-                bw_values.detectron_2 = bw_detectron_2.get_bw() """
-                
-                #bw_pub.publish(bw_values)
+               
             except:
-                pass
-                # print(type(bw_scan_0.get_bw()))
-                # print(hz_map_0.get_hz("/tb3_0/map"))
-                # hz_map_0.print_hz(["/tb3_0/map"])
-                # print("################################")
+                print(map_pt)
+                print("################################")
+                pass                
     except KeyboardInterrupt:
         rospy.loginfo("LOCAL >> Navigation compeleted!")   
 
     rospy.loginfo("LOCAL >> Starting writing the dataset in csv file ....")
-    write_dataset(str(rospy.Time.now().to_nsec()), scan_bw, map_pt, resources_data)     
+    write_dataset(str(rospy.Time.now().to_nsec()), scan_bw, map_pt, map_bw, resources_data)     
     rospy.loginfo("LOCAL >> Now dataset is ready ....")
     rospy.spin()
 
